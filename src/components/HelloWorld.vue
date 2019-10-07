9ec5e2cfa4668f257ca2e642016935056360069f
slim1616 <slim.bz@hotmail.fr>
2019-10-07 13:17:44 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          
+           <router-link class="nav-link btn btn-primary" to="/listen">Parler</router-link>
+        </div>
+         <div class="col-sm-6">
+          <router-link class="nav-link btn btn-primary" to="/listen">Chercher</router-link>
+          
+        </div>
+      </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
